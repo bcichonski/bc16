d005e4e345dc62a8ebeaa66e228f7ffae2bf6084
Bartq <bartosz.c.cichonski@gmail.com>
2018-04-02 13:51:09 +0200

    going further with code structure

--- bc16.py
+++ bc16.py
@@ -1,24 +1,12 @@
 #!/usr/bin/python3
 from bc16 import bc16_io
 from bc16 import bc16_cpu
-from bc16 import env
-
-class MemBus:
-    def __init__(self, env, size):
-        self.env = env
-        self.size = size
-        self.mem = bytearray(size)
-    def write_byte(self, addr, byte):
-        self.mem[addr] = byte
-        self.env.log("MEM WRITE AT {0:x} VAL {1:x}".format(addr, byte)) 
-    def read_byte(self, addr):
-        val = self.mem[addr]
-        self.env.log("MEM READ AT {0:x} VAL {1:x}".format(addr, val))
-        return val
+from bc16 import bc16_mem
+from bc16 import bc16_env
 
 class Bc16:
     def __init__(self, env):
-        self.mem = MemBus(env, 0x4000)
+        self.mem = bc16_mem.MemBus(env, 0x4000)
         self.io = bc16_io.IOBus()
         self.proc = bc16_cpu.Bc8181(self.mem,self.io,True)
 
@@ -26,7 +14,7 @@ class Bc16:
         self.proc.run()
 
 def main():
-    env = Environment()
+    env = bc16_env.Environment()
     computer = Bc16(env)
     computer.mem.write_byte(0x0000,0xff)
     computer.run()
---
+++ bc16/bc16_mem.py
@@ -0,0 +1,12 @@
+class MemBus:
+    def __init__(self, env, size):
+        self.env = env
+        self.size = size
+        self.mem = bytearray(size)
+    def write_byte(self, addr, byte):
+        self.mem[addr] = byte
+        self.env.log("MEM WRITE AT {0:x} VAL {1:x}".format(addr, byte)) 
+    def read_byte(self, addr):
+        val = self.mem[addr]
+        self.env.log("MEM READ AT {0:x} VAL {1:x}".format(addr, val))
+        return val
