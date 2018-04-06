00d375da45e67460e355cc77ceb84bd0ff57a594
bartq <bci@poczta.onet.pl>
2018-04-06 21:47:04 +0200

    Implementing reading and writing to tape recorder

--- bc16/bc16_io.py
+++ bc16/bc16_io.py
@@ -1,4 +1,5 @@
 from bc16 import bc16_cpu
+from random import random
 # from https://github.com/joeyespo/py-getch
 try:
     from msvcrt import getch
@@ -18,7 +19,7 @@ except ImportError:
             old = termios.tcgetattr(fd)
             termios.tcsetattr(fd, termios.TCSADRAIN, old)
 # end from
-            
+
 class IODevice:
     def __init__(self):
         self.input_port = None
@@ -53,20 +54,37 @@ class TapeRecorder(IODevice):
     TAPE4READ = 0x80
     MOVE = 0x20
     ERROR = 0x08
-    DX = 0x01
+    DX = FlagsRegister.B0
+    TX = FlagsRegister.B1
+    HALF_BYTE = 0x80
     def __init__(self, env):
         self.state = bc16_cpu.FlagsRegister(0xff)
         self.set_state(TapeRecorder.READY)
         self.env = env
         self.io_port = TapeRecorder.DEFAULT_IO_PORT
+        random.seed(None)
     def read_byte(self):
-        pass
-    def write_byte(self, byte):
-        pass
+        if(self.state.get_flag(TapeRecorder.TX)==False):
+            byte = self.file_handle.read(1)
+            if not byte:
+                byte = self.get_random()
+            self.state.set_flag(TapeRecorder.DX,
+              bool((byte & TapeRecorder.HALF_BYTE) == TapeRecorder.HALF_BYTE))
+            self.state.set_flag(TapeRecorder.DX, True)
+    def get_random(self, up=0xff):
+        return random.randint(0,up)
+    def write_byte(self):
+        if(self.state.get_flag(TapeRecorder.TX)==True):
+            bit = self.state.get_flag(TapeRecorder.DX)
+            byte = self.get_random(TapeRecorder.HALF_BYTE)
+            if bool(bit):
+                 byte = byte | TapeRecorder.HALF_BYTE
+            self.file_handle.write(byte)
+            self.state.set_flag(TapeRecorder.DX, False)
     def set_state(self, newstate):
-        
-        
         if newstate == TapeRecorder.READY:
+            if newstate == TapeRecorder.MOVE:
+                self.close()
             ready = True
             write = False
             read = False
@@ -81,7 +99,7 @@ class TapeRecorder(IODevice):
                     self.intstate = newstate
                 except:
                     error = True
-                    self.intstate = TapeRecorder.ERROR
+                    newstate = TapeRecorder.ERROR
                 ready = True
                 write = True
                 read  = False
@@ -101,7 +119,7 @@ class TapeRecorder(IODevice):
                     self.intstate = newstate
                 except:
                     error = True
-                    self.intstate = TapeRecorder.ERROR
+                    newstate = TapeRecorder.ERROR
                 ready = True
                 write = True
                 read  = False
@@ -112,14 +130,35 @@ class TapeRecorder(IODevice):
                 read  = False
                 move  = False
                 error = True
-        
+        elif newstate == TapeRecorder.MOVE:
+            if(self.intstate == TapeRecorder.TAPE4READ or
+               self.intstate == TapeRecorder.TAPE4WRITE):
+                ready = False
+                move = True
+                error = False
+            else:
+                ready = False
+                write = False
+                read = False
+                move = False
+                error = True
+
         self.intstate = newstate
         self.state.set_flag(TapeRecorder.READY, ready)
         self.state.set_flag(TapeRecorder.TAPE4WRITE, write)
         self.state.set_flag(TapeRecorder.TAPE4READ, read)
         self.state.set_flag(TapeRecorder.MOVE, move)
         self.state.set_flag(TapeRecorder.ERROR, error)
-            
+
+    def openread(self, filename):
+        self.file_handle = open(filename, "rb")
+
+    def openwrite(self, filename):
+        self.file_handle = open(filename, "wb")
+
+    def close(self):
+        self.file_handle.close()
+        self.file_handle = None
 
 class IOBus:
     def __init__(self):
