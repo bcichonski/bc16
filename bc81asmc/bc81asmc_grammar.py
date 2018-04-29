ec0ed0de0e4d88ba92e7c10a4e02953f9fa6aa41
bartq <bci@poczta.onet.pl>
2018-04-29 17:44:27 +0200

    Figured out why basic tests hangs, you cannot misuse * in parser regexes :)

--- bc81asmc/bc81asmc_grammar.py
+++ bc81asmc/bc81asmc_grammar.py
@@ -1,6 +1,7 @@
 from parsec import *
 
-whitespace = regex(r'\s*', re.MULTILINE)
+hexstr2int = lambda x: int(x, 16)
+whitespace = regex(r'\s+', re.MULTILINE)
 comment = regex(r';.*')
 ignore = many((whitespace | comment))
 
@@ -8,7 +9,8 @@ lexeme = lambda p: p << ignore
 colon = lexeme(string(':'))
 comma = lexeme(string(','))
 hexprefix = lexeme(string('0x'))
-heximm8 = lexeme(regex(r'[0-9a-fA-F]{2}')).parsecmap(int)
-heximm16 = lexeme(regex(r'[0-9a-fA-F]{4}')).parsecmap(int)
+heximm8 = lexeme(regex(r'[0-9a-fA-F]{2}')).parsecmap(hexstr2int)
+heximm16 = lexeme(regex(r'[0-9a-fA-F]{4}')).parsecmap(hexstr2int)
 
 param_imm8 = hexprefix >> heximm8
+param_imm16 = hexprefix >> heximm16
