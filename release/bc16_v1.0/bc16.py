20940e3df011fa4ab68ce4c7f655b35a744770ed
bartq <bci@poczta.onet.pl>
2024-04-13 11:32:57 +0200

    chore: revert state in released emulator

--- release/bc16_v1.0/bc16.py
+++ release/bc16_v1.0/bc16.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 
-from bc32 import bc32_io
-from bc32 import bc8182_cpu
-from bc32 import bc32_mem
-from bc32 import bc32_env
+from bc16 import bc16_io
+from bc16 import bc16_cpu
+from bc16 import bc16_mem
+from bc16 import bc16_env
 from sys import argv
 
 TOPMEM = 0x4000
@@ -11,23 +11,23 @@ TOPMEM = 0x4000
 
 class Bc16:
     def __init__(self, env, debug):
-        self.mem = bc32_mem.MemBus(env, TOPMEM)
-        self.keyboard = bc32_io.TerminalKeyboard(env)
-        self.printer = bc32_io.TerminalPrinter(env)
-        self.taperecorder = bc32_io.TapeRecorder(env)
-        self.floppydrive = bc32_io.FloppyDriveV1(env, self.mem)
-        self.io = bc32_io.IOBus()
+        self.mem = bc16_mem.MemBus(env, TOPMEM)
+        self.keyboard = bc16_io.TerminalKeyboard(env)
+        self.printer = bc16_io.TerminalPrinter(env)
+        self.taperecorder = bc16_io.TapeRecorder(env)
+        self.floppydrive = bc16_io.FloppyDriveV1(env, self.mem)
+        self.io = bc16_io.IOBus()
         self.io.add_device(self.keyboard)
         self.io.add_device(self.printer)
         self.io.add_device(self.taperecorder)
         self.io.add_device(self.floppydrive)
-        self.cpu = bc8182_cpu.Bc8181(self.mem, self.io, debug)
+        self.cpu = bc16_cpu.Bc8181(self.mem, self.io, debug)
 
     def run(self):
         self.cpu.run()
 
 def main(argv):
-    env = bc32_env.Environment()
+    env = bc16_env.Environment()
     computer = Bc16(env, len(argv) >= 3)
     if len(argv) >= 2:
         import_rom(argv[1], env, computer.mem)
